702c920cb3ebbcbd276316ba8aa2986ccf2e0135
Mirel Home <mirelalex2094@gmail.com>
2020-07-14 22:22:47 +0300

    building the article component

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import Article from '../../components/article/article.component';
+
 import './home-page.styles.scss';
 
 if (process.env.NODE_ENV !== 'production') {
@@ -12,15 +14,19 @@ class HomePage extends React.Component {
     axios({
       method: 'GET',
       url: `https://newsapi.org/v2/top-headlines?country=ro&apiKey=${process.env.REACT_APP_NEWSAPI_KEY}`,
-    }).then((res) => console.log(res.data.articles));
-
-    // console.log(process.env.REACT_APP_NEWSAPI_KEY);
+    }).then((res) => console.log(res.data.articles[0].content));
   }
 
   render() {
     return (
       <div className='homepage'>
-        <div>Kek</div>
+        <div className='homepage__articles'>
+          <Article />
+          {/* <Article />
+          <Article />
+          <Article />
+          <Article /> */}
+        </div>
       </div>
     );
   }

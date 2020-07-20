d75b4df532ab6b9d5ff5e4a513728d34f2d87e4a
Mirel Home <mirelalex2094@gmail.com>
2020-07-20 21:32:22 +0300

    building the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './App.scss';
 
 import HomePage from './pages/home-page/home-page.component';
+import NavBar from './components/navbar/navbar.component';
 
 function App() {
   return (
     <div className='App'>
+      <NavBar />
       <HomePage />
     </div>
   );

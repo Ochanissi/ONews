4035de9b533c1efa86e8aab1767a33c1c60235e9
Mirel Home <mirelalex2094@gmail.com>
2020-07-22 21:48:22 +0300

    building the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.scss';
 
@@ -11,7 +12,9 @@ function App() {
     <div className='App'>
       <NavBar />
       <Sidebar />
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }

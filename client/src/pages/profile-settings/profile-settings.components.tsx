d021121578bc81f567d83867fed31d0760bd8693
Mirel Home <mirelalex2094@gmail.com>
2020-10-15 18:51:02 +0300

    working on the profile settings page

--- client/src/pages/profile-settings/profile-settings.components.tsx
+++ client/src/pages/profile-settings/profile-settings.components.tsx
@@ -5,13 +5,27 @@ import FormInput from '../../components/form-input/form-input.component';
 import defaultLogo from '../../assets/default.png';
 
 import './profile-settings.styles.scss';
+import { connect } from 'react-redux';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { ThunkDispatch } from 'redux-thunk';
+import { AppActions } from '../../redux/store';
+import { User } from '../../redux/user/user.types';
+import { createStructuredSelector } from 'reselect';
 
-class ProfileSettings extends React.Component {
+interface ProfileSettingsProps {}
+
+interface ProfileSettingsState {}
+
+type Props = ProfileSettingsProps & LinkDispatchProps & LinkStateProps;
+
+class ProfileSettings extends React.Component<Props, ProfileSettingsState> {
   handleChange = () => {
     console.log('lel');
   };
 
   render(): JSX.Element {
+    console.log(this.props);
+
     return (
       <div className='profile-settings'>
         <div className='profile-settings__content'>
@@ -24,9 +38,36 @@ class ProfileSettings extends React.Component {
             <div className='profile-settings-block'>
               <h3 className='profile-settings-block__header'>Public info</h3>
               <h4 className='profile-settings-block__sub-header'>
-                Some info may be visible to other people using ONews services .
+                Some info may be visible to other people using ONews services.
               </h4>
               <div className='profile-settings-block__content'>
+                <div className='form-input__photo--container'>
+                  <label className='form-input__label--profile'>photo</label>
+                  <div className='form-input__photo'>
+                    <img
+                      className='form-input__photo--image'
+                      src={defaultLogo}
+                      alt='Current User'
+                    />
+
+                    <input
+                      type='file'
+                      id='files'
+                      className='form-input__photo--text'
+                      name='photo'
+                      accept='image/*'
+                      placeholder='••••••••'
+                    />
+                    <label
+                      htmlFor='files'
+                      className='custom-button custom-button__profile'
+                      id='form-input__photo--btn'
+                    >
+                      <ion-icon name='camera'></ion-icon>
+                    </label>
+                  </div>
+                </div>
+
                 <FormInput
                   name='name'
                   type='text'
@@ -97,34 +138,109 @@ class ProfileSettings extends React.Component {
                   ></textarea>
                 </div>
 
-                <label className='form-input__label'>Choose new photo</label>
-                <div className='form-input__photo'>
-                  <img
-                    className='form-input__photo--image'
-                    src={defaultLogo}
-                    alt='Current User'
-                  />
+                <div className='profile-settings-block__content--btns'>
+                  <button className='profile-settings-block__content--btns--1'>
+                    Clear
+                  </button>
 
-                  <input
-                    type='file'
-                    id='files'
-                    className='form-input__photo--text'
-                    name='photo'
-                    accept='image/*'
-                    placeholder='••••••••'
-                    disabled
-                  />
-                  <label
-                    htmlFor='files'
-                    className='custom-button custom-button__profile'
-                    id='form-input__photo--btn'
+                  <button
+                    className='profile-settings-block__content--btns--2'
+                    type='submit'
+                    value='Submit'
                   >
-                    Select file
-                  </label>
+                    Submit
+                  </button>
                 </div>
+              </div>
+            </div>
 
-                <div className='profile__settings--form--btn'>
-                  <CustomButton type='submit'>Save settings</CustomButton>
+            <div className='profile-settings-block'>
+              <h3 className='profile-settings-block__header'>Contact info</h3>
+              <h4 className='profile-settings-block__sub-header'>
+                This info is not shared with other people.
+              </h4>
+              <div className='profile-settings-block__content'>
+                <FormInput
+                  name='email'
+                  type='email'
+                  value={'email' || ''}
+                  handleChange={this.handleChange}
+                  required
+                  label='Email Address'
+                  placeholder='example@google.com'
+                  disabled
+                  profile
+                />
+
+                <FormInput
+                  name='email'
+                  type='email'
+                  value={'email' || ''}
+                  handleChange={this.handleChange}
+                  required
+                  label='Phone Number'
+                  placeholder='example@google.com'
+                  disabled
+                  profile
+                />
+                <div className='profile-settings-block__content--btns'>
+                  <button className='profile-settings-block__content--btns--1'>
+                    Clear
+                  </button>
+
+                  <button
+                    className='profile-settings-block__content--btns--2'
+                    type='submit'
+                    value='Submit'
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className='profile-settings-block'>
+              <h3 className='profile-settings-block__header'>Security</h3>
+              <h4 className='profile-settings-block__sub-header'>
+                Settings and recommendations to help you keep your account
+                secure.
+              </h4>
+              <div className='profile-settings-block__content'>
+                <FormInput
+                  name='email'
+                  type='email'
+                  value={'email' || ''}
+                  handleChange={this.handleChange}
+                  required
+                  label='Email Address'
+                  placeholder='example@google.com'
+                  disabled
+                  profile
+                />
+
+                <FormInput
+                  name='email'
+                  type='email'
+                  value={'email' || ''}
+                  handleChange={this.handleChange}
+                  required
+                  label='Phone Number'
+                  placeholder='example@google.com'
+                  disabled
+                  profile
+                />
+                <div className='profile-settings-block__content--btns'>
+                  <button className='profile-settings-block__content--btns--1'>
+                    Clear
+                  </button>
+
+                  <button
+                    className='profile-settings-block__content--btns--2'
+                    type='submit'
+                    value='Submit'
+                  >
+                    Submit
+                  </button>
                 </div>
               </div>
             </div>
@@ -135,4 +251,32 @@ class ProfileSettings extends React.Component {
   }
 }
 
-export default ProfileSettings;
+interface LinkDispatchProps {
+  // getUserSavedStartAsync: (email: string) => void;
+  // getUserLikedStartAsync: (email: string) => void;
+  // getUserDislikedStartAsync: (email: string) => void;
+  // getUserHiddenStartAsync: (email: string) => void;
+  // getUserSearchesStartAsync: (email: string) => void;
+}
+
+interface LinkStateProps {
+  currentUser: User;
+}
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
+
+const mapDispatchToProps = (
+  dispatch: ThunkDispatch<any, any, AppActions>
+): LinkDispatchProps => ({
+  // getUserSavedStartAsync: (email) => dispatch(getUserSavedStartAsync(email)),
+  // getUserLikedStartAsync: (email) => dispatch(getUserLikedStartAsync(email)),
+  // getUserDislikedStartAsync: (email) =>
+  //   dispatch(getUserDislikedStartAsync(email)),
+  // getUserHiddenStartAsync: (email) => dispatch(getUserHiddenStartAsync(email)),
+  // getUserSearchesStartAsync: (email) =>
+  //   dispatch(getUserSearchesStartAsync(email)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileSettings);

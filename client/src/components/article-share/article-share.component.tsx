f1ea3533d1a1e356c8e1e1bf0c162db249506d1c
Mirel Home <mirelalex2094@gmail.com>
2020-11-22 20:18:10 +0200

    building the article-share component and small changes to other components, added toggle functionality and share buttons

--- client/src/components/article-share/article-share.component.tsx
+++ client/src/components/article-share/article-share.component.tsx
@@ -1,41 +1,75 @@
 import React from "react";
 
+import Toast from "light-toast";
+
+import { FacebookShareButton, TwitterShareButton } from "react-share";
+
 import "./article-share.styles.scss";
 
-const ArticleShare = () => {
-  return (
-    <div className="article-share">
-      <div className="article-share__content">
-        <div className="article-share__content--source">
-          <h5>Yahoo News</h5>
-          <ion-icon name="close"></ion-icon>
-        </div>
-        <h4 className="article-share__content--title">
-          Democrats keep winning the popular vote. That worries them.
-        </h4>
-        <hr />
-        <div className="article-share__content--share">
-          <div className="article-share__content--share--title">
-            Share this via
+interface ArticleShareProps {
+  title: string;
+  source: string;
+  url: string;
+}
+
+class ArticleShare extends React.Component<ArticleShareProps> {
+  handleCopy = () => {
+    const { url } = this.props;
+
+    navigator.clipboard.writeText(url);
+
+    Toast.success("Link copied to clipboard!", 1000);
+  };
+
+  render(): JSX.Element {
+    const { title, source, url } = this.props;
+
+    return (
+      <div className="article-share">
+        <div className="article-share__content">
+          <div className="article-share__content--source">
+            <h5>{source}</h5>
+            <div className="article-share__close">
+              <ion-icon name="close"></ion-icon>
+            </div>
           </div>
-          <div className="article-share__content--share--links">
-            <div className="article-share__content--share--links--block">
-              <ion-icon name="link"></ion-icon>
-              <p>Copy link</p>
+          <h4 className="article-share__content--title">{title}</h4>
+          <hr />
+          <div className="article-share__content--share">
+            <div className="article-share__content--share--title">
+              Share this via
             </div>
-            <div className="article-share__content--share--links--block">
-              <ion-icon name="logo-facebook"></ion-icon>
-              <p>Facebook</p>
-            </div>
-            <div className="article-share__content--share--links--block">
-              <ion-icon name="logo-twitter"></ion-icon>
-              <p>Twitter</p>
+            <div className="article-share__content--share--links">
+              <button
+                className="article-share__content--share--links--block"
+                onClick={this.handleCopy}
+              >
+                <ion-icon name="link"></ion-icon>
+                <p>Copy link</p>
+              </button>
+              <FacebookShareButton
+                className="article-share__content--share--links--block"
+                url={url}
+                quote={title}
+              >
+                <ion-icon name="logo-facebook"></ion-icon>
+                <p>Facebook</p>
+              </FacebookShareButton>
+              <TwitterShareButton
+                className="article-share__content--share--links--block"
+                url={url}
+                title={title}
+                via={source}
+              >
+                <ion-icon name="logo-twitter"></ion-icon>
+                <p>Twitter</p>
+              </TwitterShareButton>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ArticleShare;
